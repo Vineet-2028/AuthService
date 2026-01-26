f958600c3c66dcfb7b18705d847cecc431a4a4d0
Vineet-2028 <neerajkumar.lntecc@gmail.com>
2026-01-26 18:27:29 +0530

    Update TokenController.java

--- src/main/java/com/example/authservice/authservice/controller/TokenController.java
+++ src/main/java/com/example/authservice/authservice/controller/TokenController.java
@@ -8,7 +8,7 @@ import com.example.authservice.authservice.service.JwtService;
 import com.example.authservice.authservice.service.RefreshTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
